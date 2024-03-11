5bdf5c6c519d50df12f143464073cac61ea54642
brittneyjb <36802080+brittneyjb@users.noreply.github.com>
2024-03-11 18:05:06 -0500

    updated westie practice code

--- westie_practice.py
+++ westie_practice.py
@@ -1,16 +1,17 @@
 import spotipy
-import random
 import time
+from pygame import mixer
 from spotipy.oauth2 import SpotifyOAuth
 
 # important variables
 
 user_id = # TODO: Spotify user id to go here
-laptop = # TODO: Spotify laptop device id to go here
+laptop = # TODO: Spotify laptop device id to go here 
 # can make other device id variables
 
-bb_wcs = 'spotify:playlist:2EmFhQbtOCRkb8yfFhZIoN', #Brittney giant wcs playlist
-bb_blues = 'spotify:playlist:7cINyALIfKcc6UV4b5aG54', #Brittney blues wcs playlist
+bb_wcs = 'spotify:playlist:2EmFhQbtOCRkb8yfFhZIoN' #Brittney giant wcs playlist
+bb_blues = 'spotify:playlist:7cINyALIfKcc6UV4b5aG54' #Brittney blues wcs playlist
+lucy_2_25 = 'spotify:playlist:4vHguqo8koIpFj6wflpqM5'
 
 
 #setup spotipy
@@ -42,34 +43,38 @@ def volume(vol):
         sp.volume(vol)
     except:
         print('volume error')
-    
-# TODO: important function for both randomization and to improve general in order playlist function
-def num_songs_in_playlist(playlist):
-    return -1;
-
-
-# rounds functions
 
 #TODO: Implement better
 def fadeout():
     for x in range(90, 0, -5):
         volume(x)
 
+def command_sound(cmd, wait_time):
+    mixer.init()
+    if cmd == 'discuss':
+        mixer.music.load('./spoken/discussl.mp3')
+    if cmd == 'switch':
+        mixer.music.load('./spoken/rotate.mp3')
+    mixer.music.play()
+    time.sleep(wait_time)
+
 
 
 def practice_playlist(pl, shuffle, song_len, wait_len):
     volume(100)
     sp.shuffle(shuffle, laptop)
-    #currently getting weird error, have playlist you want up before starting
-    #sp.start_playback(device_id=laptop, context_uri=pl) 
+    command_sound('discuss', wait_len)
+    sp.start_playback(device_id=laptop, context_uri=pl) 
     play()
-    for i in range(50): #arbitrary number for now
+    for i in range(10): #arbitrary number for now
         print("HERE")
         time.sleep(song_len) # 25) TESTING
         fadeout()
         pause()
-        time.sleep(wait_len) # 10) TESTING
-        if i < 49:
+        command_sound('discuss', wait_len)
+        if i < 9:
+            command_sound('switch', wait_len)
+            command_sound('discuss', wait_len)
             volume(100)
             next()
             play()
@@ -78,16 +83,17 @@ def practice_playlist(pl, shuffle, song_len, wait_len):
 
 
 print("Welcome to Spotipy WCS Practice")
-pl_choice = input("What playlist do you want? Options: blues, general\n")
-in_order = create = False if input("Play the playlist in order? (y/n)  ")[0] in 'nN' else True
-song_length = int(input("How long do you want the song to go for? (in seconds, eg 60 for 1 minute)\n"))
-wait_length = int(input('How long should I wait to play the next song? (also in seconds)\n'))
 
-playlist = ''
+# This setup is just an example
+pl_choice = input("What playlist do you want? Options: blues, general\n")
+shuffle = True #create = False if input("Shuffle playlist? (y/n)  ")[0] in 'nN' else True
+song_length = 90 #int(input("How long do you want the song to go for? (in seconds, eg 60 for 1 minute)\n"))
+wait_length = 45 #int(input('How long should I wait to play the next song? (also in seconds)\n'))
+
+pl = bb_wcs
 if pl_choice == 'blues':
     pl =  bb_blues
-else:
-    pl = bb_wcs
 
-practice_playlist(pl, in_order, song_length, wait_length)
-
+# Required calls
+mixer.init()
+practice_playlist(pl, shuffle, song_length, wait_length)
